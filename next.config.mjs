11973c11612072d294ca89e90ea36d0dd2e87841
Li Zhe <lizhe2004@gmail.com>
2024-02-07 23:24:28 +0800

    change image domain

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   images: {
     domains: [
       "oaidalleapiprodscus.blob.core.windows.net",
-      "ai-cover-images.s3.us-east-1.amazonaws.com",
+      "ai-cover-images.s3.amazonaws.com",
     ],
   },
 };
